4ebb1b12d67c05ffdb46fbd7364e3f74379ef663
Dillon935 <123113882+Dillon935@users.noreply.github.com>
2023-04-18 14:13:25 -0400

    2
    2

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, abort, redirect, render_template, request
 
+from src.models import db
+
 from src.repositories.movie_repository import movie_repository_singleton
 
 app = Flask(__name__)
 
 # TODO: DB connection
 
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:Dsy28146@localhost:3306/movies'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db.init_app(app)
+
 @app.get('/')
 def index():
+    movie_repository_singleton.create_data()
     return render_template('index.html')
 
 
@@ -46,3 +53,6 @@ def search_movies():
     if q != '':
         found_movies = movie_repository_singleton.search_movies(q)
     return render_template('search_movies.html', search_active=True, movies=found_movies, search_query=q)
+
+if __name__ == "__main__":
+    app.run(debug=True)
